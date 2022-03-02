3fe5707b4624763035ef91a0aabd6414220baa6b
BerezinaEV <100429742+BerezinaEV@users.noreply.github.com>
2022-03-02 20:58:56 +0700

    Add files via upload

---
+++ ToDoList-main/main.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('#input')
+const btn = document.querySelector('#btn')
+const result = document.querySelector('#result')
+const total = document.querySelector('#total')
+let i = 0
+
+// Добавление события - обработчик нажатия на кнопку
+btn.addEventListener('click', (e) => {
+    //result.innerHTML += `<li>${input.value}</li>`
+    if(input.value === '') return
+    createDeleteElements(input.value)
+    input.value = ''
+})
+
+// Создание и удаление дел в списке
+function createDeleteElements(value) {
+    i++
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li'
+    li.textContent = value
+
+    btn.className = 'btn'
+    btn.textContent = 'x'
+    li.appendChild(btn)
+
+    // Удаление дела
+    btn.addEventListener('click', (e) => {
+        i--
+        total.textContent = i
+        result.removeChild(li)
+    })
+
+    // Переключение активной строки (выбранное дело)
+    li.addEventListener('click', (e) => {
+        li.classList.toggle('li-active')
+    })
+
+    total.textContent = i
+
+    result.appendChild(li)
+}
